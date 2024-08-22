84271473048c8bd23eeab8707cc7d7ca416939c1
naman <namanchoudhary28@gmail.com>
2024-08-22 12:17:44 +0530

    server es6 dotenv setup & deploy setup

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import postRoutes from './routes/posts.js'
 import authRoutes from './routes/auth.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app=express()
 app.use(cors())
@@ -13,9 +18,14 @@ app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 app.use('/posts',postRoutes)
 app.use('/auth',authRoutes)
 
-
-const CONNECTION_URL='mongodb+srv://react:react123@cluster0.emw32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const PORT=process.env.PORT || 5000
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//now please load my static html and css files for my express app, from my /dist directory
+app.use(express.static(path.join(__dirname ,'./client/build')));
+app.get('*',function(req,res){
+res.sendFile(path.join(__dirname,'./client/build/index.html'))
+});
+const CONNECTION_URL=process.env.CONNECTION_URL
+const PORT=process.env.PORT 
 
 mongoose.connect(CONNECTION_URL)
 .then(()=>app.listen(PORT,()=>
